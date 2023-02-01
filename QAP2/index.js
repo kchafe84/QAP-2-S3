072666177e6c9ed40267c8261c98f8a510783812
Ken <107864801+kchafe84@users.noreply.github.com>
2023-02-01 08:51:12 -0330

    Add files via upload

---
+++ QAP2/index.js
@@ -0,0 +1,48 @@
+//we need a http server
+const http = require("http");
+//I like to modularize my code... so put the routes in their own file
+const routes = require("./routes.js");
+
+const server = http.createServer((request, response) => {
+  let path = "./views/";
+  // console.log(request.url, request.method);
+  // console.log(request);
+  switch (request.url) {
+    case "/":
+      path += "index.html";
+      response.statusCode = 200;
+      routes.indexPage(path, request.url, response);
+      break;
+    case "/about":
+      path += "about.html";
+      response.statusCode = 200;
+      routes.aboutPage(path, request.url, response);
+      break;
+    case "/contact":
+      path += "contact.html";
+      response.statusCode = 200;
+      routes.contactPage(path, request.url, response);
+      break;
+    case "/subscribe":
+      path += "subscribe.html";
+      response.setHeader("Set-cookie", "subscription=New");
+      routes.subscribePage(path, request.url, response);
+      break;
+
+    case "/profile":
+      path += "profile.html";
+      response.setHeader("Set-cookie", "subscription=New");
+      routes.subscribePage(path, request.url, response);
+      break;
+
+    default:
+      path += "404.html";
+      response.statusCode = 404;
+      routes.fourOfourPage(path, request.url, response);
+      break;
+  }
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening on port 3000.");
+});
